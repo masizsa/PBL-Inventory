9a9d0cb80787a84b23fd2f79795178be185caea6
Zalbaq <ihza.nk.160903@gmail.com>
2023-11-30 09:39:19 +0700

    [add] pages dir

---
+++ app/views/pages/ajukan-peminjaman/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/sidebar.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    include("../../layouts/sidebar.php");
+    ?>
+    <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias ab est, voluptate, accusamus eum dolores exercitationem dignissimos obcaecati ipsam praesentium vitae voluptatibus consectetur. Sit unde voluptates tempore non, voluptas inventore odio dolorum similique fugiat quo ipsum iusto explicabo quod ut labore neque maiores eius nisi accusantium deserunt voluptatem quia iste ab officiis! Voluptates ad in animi, ipsa maxime voluptatum magnam adipisci, placeat perferendis veniam ut repellat quasi magni doloribus itaque quidem iusto maiores eveniet necessitatibus accusamus eum beatae explicabo. Ut sit cum sint exercitationem corrupti incidunt obcaecati fugit reprehenderit ullam. Sapiente aspernatur eligendi accusamus et ut, illo quasi nemo cumque.</h1>
+
+</body>
+<script src="../../layouts/sidebar.js"></script>
+
+</html>
