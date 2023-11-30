e4517564fba48dd2499b1b3e4af15aebead6053a
Zalbaq <ihza.nk.160903@gmail.com>
2023-11-30 09:38:55 +0700

    [make] interactivity file for deploy

--- app/views/layouts/sidebar.php
+++ app/views/layouts/sidebar.php
@@ -1,35 +1,170 @@
-<!DOCTYPE html>
-<html lang="en">
+<div class="sidebar ">
+    <div class="header-sidebar">
+        <div class="logo">
+            <div class="main-icon">
+                <svg width="451" height="451" viewBox="0 0 451 451" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M404 48.4342C404 62.4893 397.62 75.141 387.445 83.9887C385.619 85.7981 383.558 87.4366 381.285 88.8684C380.004 89.6663 378.921 90.2291 378.368 90.5069C378.254 90.5639 377.436 90.9699 377.436 90.9699L251.759 152.875C250.107 153.751 249.137 154.278 249.024 154.35C248.501 154.699 248.001 155.069 247.516 155.461L189.728 183.963C189.728 183.963 189.712 183.963 189.712 183.97L189.637 184.006C172.104 191.735 159.958 208.469 159.958 227.874C159.958 229.947 160.095 231.998 160.367 234C154.086 225.687 146.085 218.591 138.342 213.733C129.803 208.376 121.711 205.811 116.649 204.536V204.522C94.934 198.83 79 180.109 79 157.883C79 138.478 91.1456 121.744 108.678 114.015L108.754 113.98C108.754 113.98 108.762 113.972 108.769 113.972L327.489 6.09078C328.224 5.7061 328.974 5.33567 329.732 4.9866L330.171 4.76577C332.831 3.455 335.589 2.50043 338.385 1.85929C342.863 0.655382 347.598 0 352.485 0C380.936 0 404 21.6846 404 48.4342Z" fill="url(#paint0_linear_227_2177)" />
+                    <path d="M287.351 246.653V246.639C282.282 245.365 274.19 242.803 265.651 237.45C257.945 232.618 249.982 225.571 243.716 217.315C242.632 215.222 241.647 213.03 240.791 210.71C234.366 193.264 233.919 166.111 247.823 155L190.034 183.478C190.034 183.478 190.019 183.478 190.019 183.485L189.943 183.52C172.41 191.243 160.265 207.962 160.265 227.351C160.265 229.422 160.401 231.472 160.674 233.472C166.319 240.931 170.569 249.38 171.986 258.07C172.895 263.6 172.653 269.231 170.895 274.768C169.577 278.896 166.144 286.996 155.817 295.032C120.699 312.313 18.3814 365.276 16.5553 367.083C6.37968 375.923 0 388.564 0 402.607C0 429.334 23.0638 451 51.5148 451C56.4018 451 61.1373 450.345 65.6152 449.142C68.4111 448.502 71.169 447.548 73.8285 446.238L74.268 446.018C75.0256 445.669 75.7757 445.299 76.5107 444.914L295.231 337.125C295.231 337.125 295.246 337.125 295.246 337.118L295.322 337.083C312.854 329.36 325 312.641 325 293.252C325 271.045 309.066 252.34 287.358 246.653H287.351Z" fill="url(#paint1_linear_227_2177)" />
+                    <defs>
+                        <linearGradient id="paint0_linear_227_2177" x1="404" y1="117" x2="79" y2="117" gradientUnits="userSpaceOnUse">
+                            <stop offset="0.149372" stop-color="#F49E4C" />
+                            <stop offset="0.649094" stop-color="#F8EDAF" />
+                        </linearGradient>
+                        <linearGradient id="paint1_linear_227_2177" x1="324.992" y1="303.004" x2="-0.00757426" y2="303.004" gradientUnits="userSpaceOnUse">
+                            <stop offset="0.415163" stop-color="#F49E4C" />
+                            <stop offset="1" stop-color="#F8EDAF" />
+                        </linearGradient>
+                    </defs>
+                </svg>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/test.css">
-    <title>SIVENTI</title>
-</head>
-
-<body>
-    <div class="sidebar">
-        <div class="header-sidebar">
-            <div class="logo">
-                <img src="../../../public/assets/Logo SIVENTI.svg" alt="">
-                <label for="">SIVENTI</label>
             </div>
-            <img src="../../../public/assets/close-circle.svg" alt="">
+            <label for="">SIVENTI</label>
         </div>
-        <div class="body-sidebar">
-            <ul class="list-menu">
-                <li><a class="" href="http://localhost/dasarWeb/PBL-Inventory/app/views/layouts/sidebar.php?page=ajukan-peminjaman"><img src="../../../public/assets/box-icon.svg" alt="">Anjukan Peminjaman</a></li>
-                <li><a class="" href="http://localhost/dasarWeb/PBL-Inventory/app/views/layouts/sidebar.php?page=barang-dipinjam"><img src="../../../public/assets/note.svg" alt="">Barang Dipinjam</a></li>
-                <li><a class="" href="http://localhost/dasarWeb/PBL-Inventory/app/views/layouts/sidebar.php?page=riwayat"><img src="../../../public/assets/history.svg" alt="">Riwayat</a></li>
-                <li><a class="" href="http://localhost/dasarWeb/PBL-Inventory/app/views/layouts/sidebar.php?page=sandi"><img src="../../../public/assets/key.svg" alt="">Ubah Sandi</a></li>
-            </ul>
-            <ul class="list-menu">
-                <li><a class="" href="http://localhost/dasarWeb/PBL-Inventory/app/views/layouts/sidebar.php?page=keluar"><img src="../../../public/assets/logout.svg" alt="">Keluar</a></li>
-            </ul>
+        <div class="close-icon">
+            <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 22.7666C17.5 22.7666 22 18.2666 22 12.7666C22 7.2666 17.5 2.7666 12 2.7666C6.5 2.7666 2 7.2666 2 12.7666C2 18.2666 6.5 22.7666 12 22.7666Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M9.16992 15.5966L14.8299 9.93658" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M14.8299 15.5966L9.16992 9.93658" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
         </div>
-    </div>
-</body>
-<script src="sidebar.js"></script>
 
-</html>
+    </div>
+    <div class="body-sidebar">
+        <ul class="list-menu">
+            <li>
+                <a class="" href="http://localhost/dasarWeb/PBL-Inventory/app/main.php?page=ajukan-peminjaman">
+                    <div class="list-icon">
+                        <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M16.8416 7.10463L10.4249 10.8213C10.1666 10.9713 9.8416 10.9713 9.57494 10.8213L3.15827 7.10463C2.69993 6.83796 2.58327 6.21296 2.93327 5.8213C3.17493 5.5463 3.44993 5.3213 3.7416 5.16296L8.25827 2.66296C9.22494 2.1213 10.7916 2.1213 11.7583 2.66296L16.2749 5.16296C16.5666 5.3213 16.8416 5.55463 17.0833 5.8213C17.4166 6.21296 17.2999 6.83796 16.8416 7.10463Z" stroke="white" />
+                            <path d="M9.52507 12.3713V18.0546C9.52507 18.6879 8.8834 19.1046 8.31673 18.8296C6.60006 17.9879 3.7084 16.4129 3.7084 16.4129C2.69173 15.8379 1.8584 14.3879 1.8584 13.1963V8.89627C1.8584 8.23793 2.55007 7.82127 3.11673 8.14627L9.1084 11.6213C9.3584 11.7796 9.52507 12.0629 9.52507 12.3713Z" stroke="white" />
+                            <path d="M10.4751 12.3713V18.0546C10.4751 18.6879 11.1168 19.1046 11.6834 18.8296C13.4001 17.9879 16.2918 16.4129 16.2918 16.4129C17.3084 15.8379 18.1418 14.3879 18.1418 13.1963V8.89627C18.1418 8.23793 17.4501 7.82127 16.8834 8.14627L10.8918 11.6213C10.6418 11.7796 10.4751 12.0629 10.4751 12.3713Z" stroke="white" />
+                        </svg>
+                    </div>
+                    Ajukan Peminjaman
+                </a>
+            </li>
+            <li>
+                <a class="" href="http://localhost/dasarWeb/PBL-Inventory/app/main.php?page=barang-dipinjam">
+                    <div class="list-icon">
+                        <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g opacity="1">
+                                <path d="M7.50008 18.9213H12.5001C16.6667 18.9213 18.3334 17.2546 18.3334 13.088V8.08797C18.3334 3.92131 16.6667 2.25464 12.5001 2.25464H7.50008C3.33341 2.25464 1.66675 3.92131 1.66675 8.08797V13.088C1.66675 17.2546 3.33341 18.9213 7.50008 18.9213Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M13.125 8.08795H6.875" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M13.125 13.088H6.875" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            </g>
+                        </svg>
+
+                    </div>
+                    Barang Dipinjam
+                </a>
+            </li>
+            <li>
+                <a class="" href="http://localhost/dasarWeb/PBL-Inventory/app/main.php?page=riwayat">
+                    <div class="list-icon">
+                        <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g opacity="1">
+                                <path d="M18.3334 10.588C18.3334 15.188 14.6001 18.9213 10.0001 18.9213C5.40008 18.9213 1.66675 15.188 1.66675 10.588C1.66675 5.98797 5.40008 2.25464 10.0001 2.25464C14.6001 2.25464 18.3334 5.98797 18.3334 10.588Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M13.0917 13.238L10.5083 11.6963C10.0583 11.4296 9.69165 10.788 9.69165 10.263V6.84631" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            </g>
+                        </svg>
+
+                    </div>
+                    Riwayat
+                </a>
+            </li>
+            <li>
+                <a class="" href="http://localhost/dasarWeb/PBL-Inventory/app/main.php?page=sandi">
+                    <div class="list-icon">
+                        <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g opacity="1">
+                                <path d="M16.4917 13.0296C14.775 14.7379 12.3167 15.2629 10.1584 14.5879L6.23337 18.5046C5.95004 18.7963 5.39171 18.9713 4.99171 18.9129L3.17504 18.6629C2.57504 18.5796 2.01671 18.0129 1.92504 17.4129L1.67504 15.5963C1.61671 15.1963 1.80837 14.6379 2.08337 14.3546L6.00004 10.4379C5.33337 8.27128 5.85004 5.81295 7.56671 4.10461C10.025 1.64628 14.0167 1.64628 16.4834 4.10461C18.95 6.56295 18.95 10.5713 16.4917 13.0296Z" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M5.7417 15.163L7.65837 17.0796" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M12.0833 9.75464C12.7736 9.75464 13.3333 9.195 13.3333 8.50464C13.3333 7.81428 12.7736 7.25464 12.0833 7.25464C11.3929 7.25464 10.8333 7.81428 10.8333 8.50464C10.8333 9.195 11.3929 9.75464 12.0833 9.75464Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            </g>
+                        </svg>
+
+                    </div>
+                    Ubah Sandi
+                </a>
+            </li>
+        </ul>
+        <ul class="list-menu">
+            <li>
+                <a class="" href="http://localhost/dasarWeb/PBL-Inventory/app/main.php?page=keluar">
+                    <div class="list-icon">
+                        <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g opacity="1">
+                                <path d="M7.41675 6.8C7.67508 3.8 9.21675 2.575 12.5917 2.575H12.7001C16.4251 2.575 17.9167 4.06667 17.9167 7.79167V13.225C17.9167 16.95 16.4251 18.4417 12.7001 18.4417H12.5917C9.24175 18.4417 7.70008 17.2333 7.42508 14.2833" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M12.4999 10.5H3.0166" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M4.87492 7.70833L2.08325 10.5L4.87492 13.2917" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            </g>
+                        </svg>
+
+                    </div>
+                    Keluar
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+<script>
+    const sidebarMenu = document.querySelectorAll(".list-menu > li > a ");
+    const selectMenu = () => {
+        const sidebarIcon = document.querySelectorAll(".list-menu > li > a > svg");
+        sidebarMenu.forEach((element) => {
+            element.addEventListener('mouseover', ({
+                target
+            }) => {
+                target.classList.add('hover-menu')
+
+            })
+            element.addEventListener('mouseout', ({
+                target
+            }) => {
+                target.style.backgroundColor = ''
+                target.classList.remove('hover-menu')
+
+            })
+        })
+
+        const urlParamsString = window.location.search;
+        const urlParams = new URLSearchParams(urlParamsString);
+        const paramValue = urlParams.get('page')
+
+        switch (paramValue) {
+            case 'ajukan-peminjaman':
+                sidebarMenu[0].classList.add('active-menu');
+                break;
+            case 'barang-dipinjam':
+                sidebarMenu[1].classList.add('active-menu');
+                break;
+            case 'riwayat':
+                sidebarMenu[2].classList.add('active-menu');
+                break;
+            case 'sandi':
+                sidebarMenu[3].classList.add('active-menu');
+                break;
+            case 'keluar':
+                sidebarMenu[4].classList.add('active-menu');
+                break;
+            default:
+                break;
+        }
+
+    }
+    selectMenu();
+
+    const sidebarControl = () => {
+        const sidebar = document.querySelector('.sidebar')
+        const closeIcon = document.querySelector('.close-icon');
+
+        closeIcon.addEventListener('click', () => {
+            sidebar.classList.toggle('minimize-sidebar')
+            sidebarMenu.forEach((element) => {
+                element.classList.toggle('hide-menu')
+            })
+        })
+    }
+
+    sidebarControl()
+</script>
