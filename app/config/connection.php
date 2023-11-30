7dabddf87e611333be2206fa3529d9c993159ae1
Zalbaq <ihza.nk.160903@gmail.com>
2023-11-30 09:40:29 +0700

    [move] connection configuration

--- app/config/connection.php
+++ app/config/connection.php
@@ -1,14 +1,12 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "inventory";
 
-
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "inventory";
-
-    //Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    //Check connection
-    if($conn->connect_error){
-        die("Connection failed : " .$conn->connect_error);
-    }
+//Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+//Check connection
+if ($conn->connect_error) {
+    die("Connection failed : " . $conn->connect_error);
+}
