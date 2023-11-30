2fd60237741a73adf9e479cfcb45379b31b72f01
Zalbaq <ihza.nk.160903@gmail.com>
2023-11-30 09:36:37 +0700

    [add] entry point for website

---
+++ app/main.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./main.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="left">
+            <?php
+            include("./views/layouts/sidebar.php");
+            ?>
+        </div>
+        <div class="right">
+            <?php
+            include("./test.php");
+            ?>
+        </div>
+    </div>
+</body>
+<script src="./views/layouts/sidebar.js"></script>
+
+</html>
