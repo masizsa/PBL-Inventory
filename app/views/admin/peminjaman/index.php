f3cceff9232e09bd44df3c02d781a349b824d11b
putricha_ <putrinorchasana@gmail.com>
2023-12-11 20:02:31 +0700

    [add] peminjaman admin

--- app/views/admin/peminjaman/index.php
+++ app/views/admin/peminjaman/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
     <link rel="stylesheet" href="../../css/adminPeminjaman.css">
     <title>Peminjaman</title>
 </head>
+
 <body>
     <div class="custom--container">
         <div class="custom--content">
@@ -32,6 +34,8 @@
                     </div>
                 </div>
 
+                <!-- ... (Bagian PHP yang telah ada sebelumnya) ... -->
+
                 <div class="custom--table-info" id="table-peminjaman">
                     <h2>Peminjaman</h2>
                     <div class="custom--table-wrapper">
@@ -45,73 +49,51 @@
                                     <th>Aksi</th>
                                 </tr>
                             </thead>
-    
                             <tbody>
-                                <tr>
-                                    <td>200123</td>
-                                    <td>kk1212</td>
-                                    <td>asu</td>
-                                    <td class="custom--first-child">aku</td>
-                                    <td rowspan="3" class="custom--confirm">
-                                        <div class="custom--aksi">
-                                            <button class="button-confirm">Konfirmasi</button>
-                                            <button class="button-info">
-                                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" style="vertical-align: middle;" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M9.99984 18.9583C5.05817 18.9583 1.0415 14.9417 1.0415 9.99999C1.0415 5.05832 5.05817 1.04166 9.99984 1.04166C14.9415 1.04166 18.9582 5.05832 18.9582 9.99999C18.9582 14.9417 14.9415 18.9583 9.99984 18.9583ZM9.99984 2.29166C5.74984 2.29166 2.2915 5.74999 2.2915 9.99999C2.2915 14.25 5.74984 17.7083 9.99984 17.7083C14.2498 17.7083 17.7082 14.25 17.7082 9.99999C17.7082 5.74999 14.2498 2.29166 9.99984 2.29166Z" fill="#121212" />
-                                                    <path d="M10 11.4584C9.65833 11.4584 9.375 11.175 9.375 10.8334V6.66669C9.375 6.32502 9.65833 6.04169 10 6.04169C10.3417 6.04169 10.625 6.32502 10.625 6.66669V10.8334C10.625 11.175 10.3417 11.4584 10 11.4584Z" fill="#121212" />
-                                                    <path d="M9.99984 14.1667C9.8915 14.1667 9.78317 14.1417 9.68317 14.1C9.58317 14.0583 9.4915 14 9.40817 13.925C9.33317 13.8416 9.27484 13.7583 9.23317 13.65C9.1915 13.55 9.1665 13.4417 9.1665 13.3333C9.1665 13.225 9.1915 13.1167 9.23317 13.0167C9.27484 12.9167 9.33317 12.825 9.40817 12.7417C9.4915 12.6667 9.58317 12.6083 9.68317 12.5667C9.88317 12.4833 10.1165 12.4833 10.3165 12.5667C10.4165 12.6083 10.5082 12.6667 10.5915 12.7417C10.6665 12.825 10.7248 12.9167 10.7665 13.0167C10.8082 13.1167 10.8332 13.225 10.8332 13.3333C10.8332 13.4417 10.8082 13.55 10.7665 13.65C10.7248 13.7583 10.6665 13.8416 10.5915 13.925C10.5082 14 10.4165 14.0583 10.3165 14.1C10.2165 14.1417 10.1082 14.1667 9.99984 14.1667Z" fill="#121212" />
-                                                </svg>
-                                            </button>
-                                        </div>
-                                    </td>
-                                </tr>                           
-                                <tr>
-                                    <td>200123</td>
-                                    <td>kk1212</td>
-                                    <td>laptop</td>
-                                    <td>binatang</td>
-                                </tr>
-                                <tr>
-                                    <td>200123</td>
-                                    <td>kk1212</td>
-                                    <td>speaker</td>
-                                    <td>binatang</td>
-                                </tr>
+                                <?php
+                                $groupedData = [];
+                                foreach ($data as $item) {
+                                    $id_peminjaman = $item['id_peminjaman'];
+                                    if (!array_key_exists($id_peminjaman, $groupedData)) {
+                                        $groupedData[$id_peminjaman] = [];
+                                    }
+                                    $groupedData[$id_peminjaman][] = $item;
+                                }
 
-                                <tr>
-                                    <td>200123</td>
-                                    <td>kk1212</td>
-                                    <td>asu</td>
-                                    <td class="custom--first-child">tes-123</td>
-                                    <td rowspan="3" class="custom--confirm">
-                                        <div class="custom--aksi">
-                                            <button class="button-confirm">Konfirmasi</button>
-                                            <button class="button-info">
-                                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" style="vertical-align: middle;" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M9.99984 18.9583C5.05817 18.9583 1.0415 14.9417 1.0415 9.99999C1.0415 5.05832 5.05817 1.04166 9.99984 1.04166C14.9415 1.04166 18.9582 5.05832 18.9582 9.99999C18.9582 14.9417 14.9415 18.9583 9.99984 18.9583ZM9.99984 2.29166C5.74984 2.29166 2.2915 5.74999 2.2915 9.99999C2.2915 14.25 5.74984 17.7083 9.99984 17.7083C14.2498 17.7083 17.7082 14.25 17.7082 9.99999C17.7082 5.74999 14.2498 2.29166 9.99984 2.29166Z" fill="#121212" />
-                                                    <path d="M10 11.4584C9.65833 11.4584 9.375 11.175 9.375 10.8334V6.66669C9.375 6.32502 9.65833 6.04169 10 6.04169C10.3417 6.04169 10.625 6.32502 10.625 6.66669V10.8334C10.625 11.175 10.3417 11.4584 10 11.4584Z" fill="#121212" />
-                                                    <path d="M9.99984 14.1667C9.8915 14.1667 9.78317 14.1417 9.68317 14.1C9.58317 14.0583 9.4915 14 9.40817 13.925C9.33317 13.8416 9.27484 13.7583 9.23317 13.65C9.1915 13.55 9.1665 13.4417 9.1665 13.3333C9.1665 13.225 9.1915 13.1167 9.23317 13.0167C9.27484 12.9167 9.33317 12.825 9.40817 12.7417C9.4915 12.6667 9.58317 12.6083 9.68317 12.5667C9.88317 12.4833 10.1165 12.4833 10.3165 12.5667C10.4165 12.6083 10.5082 12.6667 10.5915 12.7417C10.6665 12.825 10.7248 12.9167 10.7665 13.0167C10.8082 13.1167 10.8332 13.225 10.8332 13.3333C10.8332 13.4417 10.8082 13.55 10.7665 13.65C10.7248 13.7583 10.6665 13.8416 10.5915 13.925C10.5082 14 10.4165 14.0583 10.3165 14.1C10.2165 14.1417 10.1082 14.1667 9.99984 14.1667Z" fill="#121212" />
-                                                </svg>
-                                            </button>
-                                        </div>
-                                    </td>
-                                </tr>                           
-                                <tr>
-                                    <td>200123</td>
-                                    <td>kk1212</td>
-                                    <td>laptop</td>
-                                    <td>binatang</td>
-                                </tr>
-                                <tr>
-                                    <td>200123</td>
-                                    <td>kk1212</td>
-                                    <td>speaker</td>
-                                    <td>binatang</td>
-                                </tr>
+                                foreach ($groupedData as $id_peminjaman => $group) {
+                                    foreach ($group as $index => $item) {
+                                        if ($index === 0) {
+                                            echo '<tr>';
+                                            echo '<td rowspan="' . count($group) . '">' . $item['tanggal_pinjam'] . '</td>';
+                                        } else {
+                                            echo '<tr>';
+                                        }
+                                        echo '<td>' . $item['kode'] . '</td>';
+                                        echo '<td>' . $item['nama_barang'] . '</td>';
+                                        echo '<td class="custom--first-child">' . $item['nama_peminjam'] . '</td>';
+                                        if ($index === 0) {
+                                            echo '<td rowspan="' . count($group) . '" class="custom--confirm">';
+                                            echo '<div class="custom--aksi">';
+                                            echo '<button class="button-confirm">Konfirmasi</button>';
+                                            echo '<button class="button-info">';
+                                            echo '<svg width="20" height="20" viewBox="0 0 20 20" fill="none" style="vertical-align: middle;" xmlns="http://www.w3.org/2000/svg">';
+                                            // ... (SVG code)
+                                            echo '</svg>';
+                                            echo '</button>';
+                                            echo '</div>';
+                                            echo '</td>';
+                                        }
+                                        echo '</tr>';
+                                    }
+                                }
+                                ?>
                             </tbody>
                         </table>
                     </div>
                 </div>
+
+                <!-- ... (Bagian HTML lainnya) ... -->
+
 
                 <div class="custom--table-info" id="table-pengembalian">
                     <h2>Pengembalian</h2>
@@ -127,14 +109,14 @@
                                     <th>Aksi</th>
                                 </tr>
                             </thead>
-    
+
                             <tbody>
                                 <tr>
                                     <td>200123</td>
                                     <td>kk1212</td>
                                     <td>asu</td>
                                     <td class="custom--first-child">aku</td>
-                                    <td rowspan ="3">
+                                    <td rowspan="3">
                                         <div class="custom--status">
                                             <div class="custom--status-value" id="status-dipinjam">
                                                 <p>Dipinjam</p>
@@ -161,7 +143,7 @@
                                             </button>
                                         </div>
                                     </td>
-                                </tr>                           
+                                </tr>
                                 <tr>
                                     <td>200123</td>
                                     <td>kk1212</td>
@@ -180,7 +162,7 @@
                                     <td>kk1212</td>
                                     <td>asu</td>
                                     <td class="custom--first-child">aku</td>
-                                    <td rowspan ="3">
+                                    <td rowspan="3">
                                         <div class="custom--status">
                                             <div class="custom--status-value" id="status-dipinjam">
                                                 <p>Dipinjam</p>
@@ -207,7 +189,7 @@
                                             </button>
                                         </div>
                                     </td>
-                                </tr>                           
+                                </tr>
                                 <tr>
                                     <td>200123</td>
                                     <td>kk1212</td>
@@ -265,6 +247,7 @@
         </div>
     </div>
 </body>
+
 </html>
 <script>
     const buttons = document.querySelectorAll(".custom--day-wrapper button");
@@ -290,7 +273,7 @@
             tables[index].style.display = 'flex';
 
             headerTitle.textContent =
-            index === 0 ? "Data Peminjaman" : "Data Pengembalian";
+                index === 0 ? "Data Peminjaman" : "Data Pengembalian";
 
             subheader.textContent = index === 0 ? "Silahkan konfirmasi peminjaman jika sudah siap!" : "Silahkan konfirmasi pengembalian jika sudah benar!";
         });
